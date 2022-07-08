93e599d48e08060768d09485529b918d9171d11a
paolomilan <milan.paolo@yahoo.com>
2022-07-08 19:45:10 -0400

    NuGet packages and Events.cs file in /Models

---
+++ MVCWebApp/Models/Events.cs
@@ -0,0 +1,17 @@
+﻿// New class created to create this Model
+using System.ComponentModel.DataAnnotations;
+
+namespace MVCWebApp.Models
+{
+    public class Events
+    {
+        //ID for database primary key
+        public int Id { get; set; }
+        public string? Event { get; set; }
+
+        [DataType(DataType.DateTime)]
+        public DateTime EventDate { get; set; }
+        public string? Guest { get; set; }
+        public decimal Price { get; set; }
+    }
+}
