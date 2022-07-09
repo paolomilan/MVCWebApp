f1179f8475e8624f9d52f0f937e73ef9083fa5e6
paolomilan <milan.paolo@yahoo.com>
2022-07-08 21:23:57 -0400

    updated namespace to MVCWebApp

--- MVCWebApp/Controllers/HelloWorldController.cs
+++ MVCWebApp/Controllers/HelloWorldController.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Text.Encodings.Web;
 
-namespace MvcMovie.Controllers
+namespace MVCWebApp.Controllers
 {
     public class HelloWorldController : Controller
     {
